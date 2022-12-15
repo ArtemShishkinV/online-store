bfbc135d11622737192ad1df14eee0ceb7df4f31
g943_shishkin.a.i <astopikov@gmail.com>
2022-12-16 00:49:15 +0300

    fix: add oauth resource server in project

--- src/main/java/com/shishkin/auctionapp/config/SecurityConfig.java
+++ src/main/java/com/shishkin/auctionapp/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.shishkin.auctionapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,13 +19,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeRequests()
+        http = http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.DELETE, "/api/store/**").hasAuthority("SCOPE_delete")
+                .antMatchers(HttpMethod.PATCH, "/api/store/**").hasAuthority("SCOPE_change")
+                .antMatchers(HttpMethod.POST, "/api/store/**").hasAuthority("SCOPE_change")
+                .antMatchers("/data-api/**").hasAuthority("SCOPE_data-api")
+                .and().oauth2ResourceServer().jwt().and().and();
+        return http.authorizeRequests()
                 .antMatchers("/**/create").hasRole("ADMIN")
                 .antMatchers( "/categories", "/products").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/", "/**").permitAll()
                 .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true)
-                .and().logout().logoutSuccessUrl("/login")
-                .and().build();
+                .and().logout().logoutSuccessUrl("/login").and().build();
     }
 
 }
